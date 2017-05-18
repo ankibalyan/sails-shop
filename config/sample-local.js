229dcaa331e98ddca7c36bf3e92f94e3c5323ede
Ankit Kumar <ankit@ipapagari.com>
2017-05-19 00:42:22 +0530

    mongodb setup

---
+++ config/sample-local.js
@@ -0,0 +1,21 @@
+/**
+ * Local environment settings
+ *
+ * Either set these values to your production file
+ * or create /config/local.js file with your own values
+ *
+ * For more information, check out:
+ * https://sailsjs.com/docs/concepts/configuration/the-local-js-file
+ */
+
+module.exports = {
+
+  // Any configuration settings may be overridden below, whether it's built-in Sails
+  // options or custom configuration specifically for your app (e.g. Stripe, Mailgun, etc.)
+  datastores: {
+    default: {
+      adapter: 'sails-mongo',
+      url: 'mongodb://user:password@host:port/database',
+    },
+  }
+};
